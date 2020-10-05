8d19706891f27c35e82f7c73990363ade3e87f4b
bellaappel <bellaappel0@gmail.com>
2020-10-05 14:09:18 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+	prime = true
+	if int <= 1
+		prime = false
+	else
+		for i in (2...int).to_a
+			if (int % i == 0)
+				prime = false
+				break
+			end
+		end
+	end
+	prime
+end 
